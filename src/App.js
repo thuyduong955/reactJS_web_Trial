fa33cb73485a02d326a4258bfef3c54c2036b04f
thuyduong955 <23520347@gm.uit.edu.vn>
2025-09-30 11:50:00 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        <b>Welcome to my blog</b>
+      </h1>
+      <p>My name is Duong and I'm so biutifun.</p>
+      <img src="/summer.jpg" alt="Summer" width="650" height="900" />
+    </div>
+  );
+}
+
+export default App;
